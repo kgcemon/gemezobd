a2461ae9132649430eb5d0d5add1ff7e70bcc973
kgcemon <kgcemonbd@gmail.com>
2025-09-20 23:43:21 -0700

    first commit

--- app/Http/Controllers/api/PaymentMethodController.php
+++ app/Http/Controllers/api/PaymentMethodController.php
@@ -9,7 +9,7 @@ use Illuminate\Http\Request;
 class PaymentMethodController extends Controller
 {
     public function index(Request $request){
-        $payment_methods = PaymentMethod::all();
+        $payment_methods = PaymentMethod::where('name', '!=', 'Wallet')->get();
         return response()->json([
             'status' => true,
             'data' => $payment_methods,
